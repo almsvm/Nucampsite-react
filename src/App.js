a52849df4c67df0a0a53e3243aa3747fd911c570
Alime Sevim <alimesevim@gmail.com>
2022-09-24 16:55:11 -0400

    Format

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import CampsiteList from './features/campsites/CampsiteList.js';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import './App.css';
+import CampsiteList from "./features/campsites/CampsiteList.js";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
-    return (
-        <div className='App'>
-            <Header />
-            <CampsiteList />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <CampsiteList />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
